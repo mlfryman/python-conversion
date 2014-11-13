280247ae4625f5f38fc3ed73d7b99f499fb78fe3
Melanie L. Fryman <melanie@frymanet.com>
2014-11-13 09:34:33 -0600

    init

---
+++ intro.py
@@ -0,0 +1,23 @@
+# this is a comment
+
+# this is the first comment
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+
+x = 3
+y = 4.2
+z = 'hello'
+
+a = "hello"
+b = x + y - x
+c = 17 / 3  # classic division returns a float
+d = 17 // 3  # floor division discards the fractional part
+e = 17 % 3  # the % operator returns the remainder of the division
+f = 5 * 3 + 2  # result * divisor + remainder
+g = 5 ** 2  # 5 squared
+h = 2 ** 7  # 2 to the power of 7
+
+
+print(x, y, z, a, b, c, d, e, f, g, h)
---
+++ start.py
@@ -0,0 +1,10 @@
+age = 20
+gender = 'female'
+
+if age >= 21 and (gender == 'female' or gender == 'male'):
+   print('go have a drink')
+else:
+   print('go have your parents buy you a drink')
+
+for x in range(20, -50, -5):
+    print("{0} to the {1} power is {2}.format(x, 2, x**2)")
