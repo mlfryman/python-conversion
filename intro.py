d5cd7be230e5604295b79c39d920637396d246d2
Melanie L. Fryman <melanie@frymanet.com>
2014-11-14 09:27:51 -0600

    cleaned up code

--- intro.py
+++ intro.py
@@ -5,7 +5,6 @@ spam = 1  # and this is the second comment
           # ... and now a third!
 text = "# This is not a comment because it's inside quotes."
 
-
 x = 3
 y = 4.2
 z = 'hello'
@@ -18,6 +17,5 @@ e = 17 % 3  # the % operator returns the remainder of the division
 f = 5 * 3 + 2  # result * divisor + remainder
 g = 5 ** 2  # 5 squared
 h = 2 ** 7  # 2 to the power of 7
-
 
 print(x, y, z, a, b, c, d, e, f, g, h)
